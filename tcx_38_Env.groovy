80a3634c01fcaabdd6ef2d9163bb35bd7f82777a
Thejeswarareddy R <theja0473@users.noreply.github.com>
2020-07-20 21:07:33 +0530

    Update tcx_38_Env.groovy
    Adding function

--- tcx_38_Env.groovy
+++ tcx_38_Env.groovy
@@ -1,4 +1,10 @@
-def Micro = 'tcx_38'
-def python_exe_path = "c:\\Python27\\python.exe"
+environment{
+  Micro = 'tcx_38'
+  python_exe_path = "c:\\Python27\\python.exe"
+}
+def print_vars(){
+  println "Micro: ${env.Micro}"
+  println "Python_Path: ${env.python_exe_path}"
+}
 
 return this
