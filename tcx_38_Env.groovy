5c26fdff2542e96cc319870894556cbfbf0235e6
Thejeswarareddy R <theja0473@users.noreply.github.com>
2020-07-20 21:14:17 +0530

    Update tcx_38_Env.groovy
    Only env vars

--- tcx_38_Env.groovy
+++ tcx_38_Env.groovy
@@ -3,9 +3,5 @@ environment{
   Micro = 'tcx_38'
   python_exe_path = "c:\\Python27\\python.exe"
 }
-def print_vars(){
-  println "Micro: ${env.Micro}"
-  println "Python_Path: ${env.python_exe_path}"
-}
 
 return this
