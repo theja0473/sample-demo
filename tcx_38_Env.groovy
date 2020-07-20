9c72c83704e3a58d212aa577bc5ed3c372b937ff
Thejeswarareddy R <theja0473@users.noreply.github.com>
2020-07-20 20:45:58 +0530

    Update tcx_38_Env.groovy
    Updating Env file

--- tcx_38_Env.groovy
+++ tcx_38_Env.groovy
@@ -1,4 +1,4 @@
-def env_vars= [:]
-	env_vars['Micro'] = 'tcx_38'
-	env_vars['python_exe_path'] = "c:\\Python27\\python.exe"
-return env_vars;
+environment{
+	Micro = 'tcx_38'
+	python_exe_path = "c:\\Python27\\python.exe"
+}
